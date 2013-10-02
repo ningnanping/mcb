f1a38e9125a644534388d587a49cdf73fc88b90a
lupengji <lu520tang@163.com>
2013-10-02 15:03:12 +0800

    将过多的费代马删除只留下一个demo 但是不能运行  这个是oracle的一个模型 需要改成mysql的

--- mcb/src/main/test/com/cctc/framework/test/BaseTestCase.java
+++ mcb/src/main/test/com/cctc/framework/test/BaseTestCase.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @Title ：
  * @Description ：junit 单元测试的父类 没有事务回滚
  * @author ：LuFengLiang
- * @Company: 北京恒泰实达科技股份有限公司
+ * @Company: 
  * @Copyright: Copyright (c) @2012-8-10
  * @version ： 1.0
  */
