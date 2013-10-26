7e1166eddb2ae9d76b7881de41caa59a820b3ead
lupengji <lu520tang@163.com>
2013-10-26 13:54:28 +0800

    1、提交

--- mcb/src/main/java/com/xingzhe/common/mapper/UserMapper.java
+++ mcb/src/main/java/com/xingzhe/common/mapper/UserMapper.java
@@ -9,7 +9,6 @@ import com.xingzhe.common.domain.User;
 import com.xingzhe.framework.mapper.SqlMapper;
 
 import org.mybatis.caches.ehcache.LoggingEhcache;
-import org.springframework.stereotype.Component;
 
 @CacheNamespace(size = 1024,implementation = LoggingEhcache.class)
 public interface UserMapper extends SqlMapper {
